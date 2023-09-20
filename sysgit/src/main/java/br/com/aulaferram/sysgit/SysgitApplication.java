96419a72737e40734935e13f5fe3946bc990db13
Roussian Gaioso <roussian.comp@gmail.com>
2023-09-19 21:18:25 -0300

    Criação do Projeto

---
+++ sysgit/src/main/java/br/com/aulaferram/sysgit/SysgitApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulaferram.sysgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysgitApplication.class, args);
+	}
+
+}
---
+++ sysgit/src/test/java/br/com/aulaferram/sysgit/SysgitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aulaferram.sysgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
